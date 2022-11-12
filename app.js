a80193a95d6b37dfddc2ed5577518a5f25257d86
vachito <valen-charlone@hotmail.com>
2022-11-12 01:35:53 -0300

    se actualizaron cambios

--- app.js
+++ app.js
@@ -18,54 +18,59 @@ function showFunction() {
     while (respuesta === "si" && cant <= 2) {
         let producto = prompt(`
             Que tipo de producto te gustaria elegir?
-
+            
             A- ${vacuno}
             B- ${porcino}
             C- ${achura}
-        `)
-
-        if(cant <=2){
-            if (producto === "A") {
-                alert("elegiste " + vacuno);
-            } else if (producto === "B") {
-                alert("elegiste " + porcino);
-            } else {
-                alert("elegiste " + achura);
+            `)
+            
+            if(cant <=2){
+                switch(producto){
+                    case 'A':
+                        alert(`Elegiste ${vacuno}`);
+                        break
+                    case 'B':
+                        alert(`Elegiste ${porcino}`);
+                        break
+                    case 'C':
+                        alert(`Elegiste ${achura}`);
+                        break
+                    default :
+                        alert('no elegiste nada :(')
+                }
             }
-        }
+                cant++
+                if(cant === 3){
+                    alert(`
+                    -Ya hiciste la cantidad maxima de elecciones 
+                    -Fueron un total de ${cant} productos
+                    `)
+                }
+            
+                respuesta = prompt(`
+                Desea seguir elegiendo
+                
+                - si
+                - no
+                `)
 
-        cant++
-
-        respuesta = prompt(`
-            Desea seguir elegiendo
-
-            - si
-            - no
-        `)
-
-        if(cant === 3){
-            alert('ya hiciste la cantidad maxima de elecciones ')
-        }
+                alert(`-Fueron un total de ${cant} productos`)
+                
+            }
+            
+            if(respuesta === 'no'){
+                alert('Gracias por la visita!')
+            }
         
-    }
-    if(respuesta === 'no'){
-        alert('Gracias por la visita!')
-    }
-
 }
 
 showFunction();
 
-// switch(producto){
-//     case 1:
-//         alert("elegiste Carne vacuna");
-//         break
-//     case 2:
-//         alert("elegiste Carne porcina");
-//         break
-//     case 3:
-//         alert("elegiste achuras");
-//         break
-//     default :
-//         alert('no elegiste nada :(')
-// }
+// if (producto === "A") {
+//     alert("elegiste " + vacuno);
+// } else if (producto === "B") {
+//     alert("elegiste " + porcino);
+// } else if (producto === 'C'){
+//     alert("elegiste " + achura);
+// }else{
+// alert('Ese prodcuto no existe!')
