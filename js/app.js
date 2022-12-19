0eba3c53cb010ce5bc1a82f819ad59403cb40e0e
vachito <valen-charlone@hotmail.com>
2022-12-19 19:37:14 -0300

    Se termino tercer entrega Curso JS

--- js/app.js
+++ js/app.js
@@ -1,94 +1,168 @@
-alert("Bienvenido a carnes premiun!");
-
-
-let vacuno = 'Carne vacuna'
-let porcino = 'Carne porcina'
-let achura = 'Achura'
-
-let cant = 0;
-
-function showFunction() {
-    let respuesta = prompt(`
-        Hola cliente, desea continuar y ver que estamos vendiendo?
-
-        - si
-        - no
-    `);
-
-    while (respuesta === "si" && cant <3) {
-        let producto = prompt(`
-            Que tipo de producto te gustaria elegir?
-            
-            A- ${vacuno}
-            B- ${porcino}
-            C- ${achura}
-        `)
-        if (producto) {
-            switch(producto){
-                case 'A':
-                    alert(`Elegiste ${vacuno}`);
-                    break
-                case 'B':
-                    alert(`Elegiste ${porcino}`);
-                    break
-                case 'C':
-                    alert(`Elegiste ${achura}`);
-                    break
-                default:
-                    alert(`Opcion incorrecta`)
-                    break
-            }
-            cant++
-        } 
-        
-        else{
-            alert('no elegiste nada :(')
-        }
-
-        respuesta = prompt(`
-        Quieres seguir comprando?
-        
-        - si
-        - no
-        `)
-
-        if (respuesta === 'no'){
-            alert(`-Fueron un total de ${cant} productos`)
-        }
-        
-        if(cant === 3){
-            alert(`
-            -Ya hiciste la cantidad maxima de elecciones 
-            -Fueron un total de ${cant} productos
-            `),
-
-            alert('Gracias por la visita!') 
-        }
-        
+class Productos {
+    constructor(id, corte, price, img) {
+            this.id = id,
+            this.corte = corte,
+            this.price = price,
+            this.img = img,
+            this.cantidad = 1
     }
-   
-    while(respuesta !== 'si'){
-        if(respuesta === 'no'){
-            alert('Gracias por la visita')
-        }
-        else {
-            alert(`
-                -Opcion incorrecta!
-                -Alcanzaste a elegir ${cant} productos
-            `)
-        }
-        break
-    }
-           
 }
 
-showFunction();
+const vacio = new Productos(1, "Vacio", 1500, "img/vacio.jpg");
+const tapaDeAsado = new Productos(2, "Tapa de asado", 1450, "img/tapaDeAsado.jpg");
+const tomaHawk = new Productos(3, "Toma Hawk", 1650, "img/tomahawk.jpg");
+const picaña = new Productos(4, "Picaña", 1800, "img/picaña.jpg");
+const tiraDeAsado = new Productos(5, "Tira de Asado", 2000, "img/asado.webp")
 
-// if (producto === "A") {
-//     alert("elegiste " + vacuno);
-// } else if (producto === "B") {
-//     alert("elegiste " + porcino);
-// } else if (producto === 'C'){
-//     alert("elegiste " + achura);
-// }else{
-// alert('Ese prodcuto no existe!')
+const arrayProductos = [];
+
+arrayProductos.push(vacio);
+arrayProductos.push(tapaDeAsado);
+arrayProductos.push(tomaHawk);
+arrayProductos.push(picaña);
+arrayProductos.push(tiraDeAsado)
+
+
+let carrito = [];
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+const carritoContenedor = document.getElementById('lista-carrito')
+
+document.addEventListener("DOMContentLoaded", () => {
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    
+    if(verCarrito){
+        showCarrito()
+    }
+});
+
+if(contenedorProductos) {
+
+    const showProductos = () => {
+        
+        arrayProductos.forEach(prod => {
+            const { id, corte, price, img } = prod;
+            const card = document.createElement("div");
+            card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+            card.innerHTML = `
+                <div class = "shop-card">
+                    <h5> ${corte} </h5>
+                    <div class = "slider">
+                        <img src="${img}">
+                    </div>
+                    <div class="cta">
+                        <p class="price">$ ${price} </p>
+                        <button class="btn" id = "boton${id}" >Agregar al Carrito</button>
+                        </div>
+                        </div>`
+                    
+            //agregar al productos al carrito
+            contenedorProductos.appendChild(card)
+    
+            const btn = document.getElementById(`boton${id}`)
+            btn.addEventListener('click', () => {
+                addToCart(id)
+            })
+        })
+    }
+    showProductos()
+}
+
+
+//FUNCION AGREGAR AL CARRITO 
+
+const addToCart = (id) => {
+    const productoEnCarrito = carrito.find(prod => prod.id === id)
+    if(productoEnCarrito){
+        productoEnCarrito.cantidad++
+    } else {
+        const producto = arrayProductos.find(prod => prod.id === id)
+        carrito.push(producto)
+        Toastify({
+            text: "Se agreggo al carrito",
+            gravity: 'bottom',
+            duration: 2000
+            
+            }).showToast();
+        console.log(producto)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    }
+}
+
+//MOSTRAR CARRITO
+const verCarrito = document.getElementById('verCarrito')
+
+if(verCarrito){
+    verCarrito.addEventListener('click', () => {
+        showCarrito()
+    })
+}
+
+
+const showCarrito = () => {
+    carritoContenedor.innerHTML = ''
+    carrito.forEach(producto => {
+        
+        const { id, corte, price, img, cantidad } = producto;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        
+                        <td>
+                            <img class="img-fluid img-carrito" src="${img}"/>
+                        </td>
+                        <td>${corte}</td>
+                        <td>${price}</td>
+                        <td>${cantidad}</td>
+                        <td>
+                            <button class="btn btn-danger  m-2" id ="eliminar${id}" >Eliminar producto</button>
+                        </td>
+                        
+                    `;
+        carritoContenedor.appendChild(tr);
+
+        const boton = document.getElementById(`eliminar${id}`);
+        boton.addEventListener("click", () => {
+            deleteFromCart(id);
+        })
+    })
+}
+   
+
+const deleteFromCart = (id) => {
+    const producto = carrito.find( producto => producto.id === id);
+    const indice = carrito.indexOf(producto);
+    carrito.splice(indice, 1);
+
+    showCarrito();
+ 
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+
+const vaciarCarrito = document.getElementById('vaciarCarrito')
+vaciarCarrito.addEventListener('click', () => {
+    emptyCart()
+})
+
+const emptyCart = () => {
+    carrito.length = []
+    showCarrito()
+
+    localStorage.clear()
+}
+
+const total = document.getElementById("total");
+
+const calculateTotal = () => {
+    let totalCompra = 0;
+    carrito.forEach(producto => {
+        totalCompra += producto.price * producto.cantidad;
+    })
+    total.innerHTML = `Total: $${totalCompra}`;
+}
+
+
+
+
+
+
--- js/productos.js
+++ js/productos.js
@@ -1,95 +1,196 @@
 class Productos {
-
-    constructor(order, achura, corte, total){
-
-        this.order = order,
-        this.achura = achura,
+  constructor(id, corte, price, img) {
+        this.id = id,
         this.corte = corte,
-        this.total = total
+        this.price = price,
+        this.img = img,
+        this.cantidad = 1
+  }
+}
+
+const pedido1 = new Productos(1, "Vacio", 1500, "img/vacio.jpg");
+const pedido2 = new Productos(2, "Tapa de asado", 1450, "img/tapaDeAsado.jpg");
+const pedido3 = new Productos(3, "Toma Hawk", 1650, "img/tomahawk.jpg");
+const pedido4 = new Productos(4, "Picaña", 1800, "img/picaña.jpg");
+
+const arrayProductos = [];
+
+arrayProductos.push(pedido1);
+arrayProductos.push(pedido2);
+arrayProductos.push(pedido3);
+arrayProductos.push(pedido4);
+
+
+let carrito = [];
+
+
+const productos = document.getElementById("contenedor-productos");
+const contenedorCarrito = document.getElementById('carritoContenedor')
+const vaciarCarrito = document.getElementById('vaciarCarrito')
+const precioTotal = document.getElementById("precioTotal");
+const activarFuncion = document.getElementById('activarFuncion')
+const procesarCompra = document.getElementById('procesarCompra')
+
+if(activarFuncion) {
+    activarFuncion.addEventListener('click', compraCarrito)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    
+
+    mostrarCarrito()
+    if(activarFuncion){
+        document.getElementById("activarFuncion").click(compraCarrito);
+    }
+    
+})
+
+if (vaciarCarrito) {
+    vaciarCarrito.addEventListener("click", () => {
+      carrito.length = [];
+      mostrarCarrito();
+    });
+  }
+
+if(procesarCompra){
+    procesarCompra.addEventListener("click", () => {
+        if (carrito.length === 0) {
+          Swal.fire({
+            title: "¡Tu carrito está vacio!",
+            text: "Compra algo para continuar con la compra",
+            icon: "error",
+            confirmButtonText: "Aceptar",
+          });
+        } else {
+          location.href = "carrito.html";
+        }
+    });
+}
+
+//CONTENEDOR PRODUCTOS
+
+arrayProductos.forEach((producto) => {
+    const { id, corte, price, img, cantidad } = producto;
+    if(productos){
+        productos.innerHTML += `
+                <div class = "shop-card">
+                    <h5> ${corte} </h5>
+                    <div class = "slider">
+                        <img src="${img}">
+                    </div>
+                    <div class="cta">
+                        <p class="price">$ ${price} </p>
+                        <p class="card-text">Cantidad: ${cantidad}</p>
+                        <button class="btn" id = "boton${id}" >Agregar al Carrito</button>
+                    </div>
+                </div>`;
+    
+            const boton = document.getElementById(`boton${id}`);
+            boton.addEventListener("click", () => {
+                addToCart(id);
+            })
+    }
+})
+
+
+//AGREGAR PRODUCTOS CARRITO 
+
+const addToCart = (id) => {
+    const productInCard = carrito.find((producto) => producto.id === id);
+    if(productInCard){
+        productInCard.cantidad++
+    } else {
+        const producto = arrayProductos.find((producto) => producto.id === id);
+        carrito.push(producto);
         
     }
-  
-}
-
-const pedido1 = new Productos(1, 'Chorizo','Vacio', 1500)
-const pedido2 = new Productos(2, 'Chinchulin', 'Tapa de asado', 1450)
-const pedido3 = new Productos(3, 'Morcilla y Riñon', 'Asado', 1650)
-const pedido4 = new Productos(4, 'Mollejas', 'Lomo', 1800)
-
-const arrayPedidos = []
-
-arrayPedidos.push(pedido1)
-arrayPedidos.push(pedido2)
-arrayPedidos.push(pedido3)
-arrayPedidos.push(pedido4)
-
-
-const precioAchura = 500
-const precioCarne = 1000
-
-function showMenu() {
-    alert("Bienvenido a Carnes Premiun")
-    let pregunta = parseInt(prompt(`
-        Elija una opcion!
-        1) Realizar un pedido
-        2) Modificar un pedido
-        3) Seleccionar pedido exitente
-        4) Salir
-    `))
-    return pregunta
-}
-
-function makeAnOrder() {
-    let achura = prompt('Ingrese achura que desee consumir')
-    let corte = prompt('Ingrese el corte de carne que desee consumir')
-    let total = parseInt(alert(`El total es ` + (precioAchura + precioCarne)))
-    let pedidoNuevo = new Productos(achura, corte, total)
-    arrayPedidos.push(pedidoNuevo)
-    console.log(arrayPedidos)
-}
-
-function updateOrder() {
-    let order = parseInt(prompt('Ingrese el numero de orden'))
-    let indice = arrayPedidos.find(indice => indice.order === order)
-    let index = arrayPedidos.indexOf(indice)
-    let achura = prompt('Ingrese achura nueva')
-    let corte = prompt('Ingrese corte nuevo')
-    let total = parseInt (alert(`El total es ` + (precioAchura + precioCarne)))
-    let pedidoModificado = new Productos(order, achura, corte, total)
-    arrayPedidos.splice(index, 1, pedidoModificado)
-    console.log(arrayPedidos)
-}
-
-function showOrders() {
-    let newArray = arrayPedidos
-    for (let index = 0; index < newArray.length; index++) {
-        const element = newArray[index];
-        console.log(element)
+    mostrarCarrito()
+};
+const mostrarCarrito = () => {
+    const body = document.querySelector(".modal #modal-body");
+    if (body) {
+        body.innerHTML = "";
+        carrito.forEach((prod) => {
+        const { id, corte, price, cantidad } = prod;
+        body.innerHTML += `
+            <div class="modal-contenedor">
+                <div>
+                    <p>Producto: ${corte}</p>
+                    <p>Precio: ${price}</p>
+                    <p>Cantidad :${cantidad}</p>
+                    <button class="btn btn-danger" id = "boton${id}">Eliminar producto</button>
+                </div>
+            </div>
+    
+        `;
+        const boton = document.getElementById(`boton${id}`)
+        boton.addEventListener('click', () => {
+            eliminarDelCarrito(id)
+        })
+      });
     }
-    let order = parseInt(prompt('Ingrese el numero de orden'))
-    let indice = arrayPedidos.find(indice => indice.order === order)
-    console.log(indice)
+  
+    // if (carrito.length === 0) {
+    //   console.log("Nada");
+    //   body.innerHTML = `
+    //   <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
+    //   `;
+    // } else {
+    //   console.log("Algo");
+    // }
+    contenedorCarrito.textContent = carrito.length;
+  
+    // if (precioTotal) {
+    //   precioTotal.innerText = carrito.reduce(
+    //     (acc, prod) => acc + prod.cantidad * prod.precio,
+    //     0
+    //   );
+    // }
+    guardarStorage()
+    
+}
+//Función que elimina el producto del carrito: 
+
+const eliminarDelCarrito = (id) => {
+    // const producto = carrito.find( producto => producto.id === id);
+    // const indice = carrito.indexOf(producto);
+    // carrito.splice(indice, 1);
+    const juegoId = id;
+    carrito = carrito.filter((juego) => juego.id !== juegoId);
+    //localStorage: 
+    mostrarCarrito()
+    
 }
 
-function leave(){
-    alert('Gracias por probar esta simulacion')
+const guardarStorage = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 
-let menu = showMenu()
 
-if(menu === 1){
-    makeAnOrder()
 
-} else if( menu === 2){
-    updateOrder()
-
-}
-else if(menu === 3){
-    showOrders()
-}
-else{
-    leave()
+const compraCarrito = () => {
+    
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    
+    carrito.forEach((prod) => {
+            const { id, corte, price, img, cantidad } = prod;
+            if (listaCompra) {
+              const tr = document.createElement("tr");
+              row.innerHTML += `
+                        <td>
+                        <img class="img-fluid img-carrito" src="${img}"/>
+                        </td>
+                        <td>${corte}</td>
+                        <td>${price}</td>
+                        <td>${cantidad}</td>
+                        <td>${price * cantidad}</td>
+                    `;
+              listaCompra.appendChild(tr);
+            }
+          });
+    guardarStorage()
 }
 
 
